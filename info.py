3f1a365915486abe85b7a4a3eb8614d88d0ee48c
dotxbrain <infodotxbrain@gmail.com>
2025-07-25 17:36:38 +0530

    Update info.py

--- info.py
+++ info.py
@@ -22,8 +22,7 @@ USERNAME = environ.get('USERNAME', "https://telegram.me/dotxbrain")
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '--1002823302885'))
 MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/+GMisvnc6K19kMGZl')
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002842829182').split()]
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://infodotxbrain:bq3jxV3MFfHNNztx@cluster0.nrlf6aw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
-")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://infodotxbrain:bq3jxV3MFfHNNztx@cluster0.nrlf6aw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 DATABASE_NAME = environ.get('DATABASE_NAME', "dotxbrain")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
